8d5cd15809ed44d517fda4a744420c5053673e72
Alessandro De Martini <alessandro.demartini@garmin.com>
2023-03-06 18:24:10 +0100

    change build image scripts

--- maven/3.8.7/openjdk17/build-image.sh
+++ maven/3.8.7/openjdk17/build-image.sh
@@ -1,5 +1,5 @@
 WD_DOCKER="`cd ./maven/3.8.7/openjdk17/;pwd`"
-IMAGE_ID=navionics/${projectName}:${imageVersion}
+IMAGE_ID=${imageName}:${imageVersion}
 
 cd ${WD_DOCKER}
 docker build -t ${IMAGE_ID} .
